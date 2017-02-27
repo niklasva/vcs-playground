60cb7af1dfdf6410a7517b17335a810478d4ef57
Niklas Vågstedt <niklasva@gmail.com>
2017-02-27 07:18:13 +0100

    sortof

--- kattis/sort-of-sorting/sort-of-sorting.cpp
+++ kattis/sort-of-sorting/sort-of-sorting.cpp
@@ -8,96 +8,29 @@ using namespace std;
 struct 
 {
     bool operator()(string a, string b)
-    {   
-        if (a[0] < b[0])
-        {
-            return true;
-        }
-        if (a[0] == b[0])
-        {
-            if (a[1] < b[1])
-                return true;
-        }        
-        return false;
+    {           
+        // Sortera bara enligt de två första tecknen.
+        return a.substr(0,2) < b.substr(0,2);
     }
     
 } sort_of_sort;
 
-struct 
-{
-    bool operator()(string a)
-    {   
-        // Se till att namnet uppfyller kraven.
-        if (a.size() < 2 || a.size() > 20)
-        {
-            return true;
-        }
-        for (char c : a)
-        {
-            if (!((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-    
-} check_name;
-
 int main()
 {
     int count;
-    unsigned test_count {0};
     string delim {""};
     while(cin >> count)
     {
         if (count == 0)
             break;
 
-        else if (count < 0 || count > 200) // Ignorera nästa testfall om count är felaktig.
-        {
-            // Släng bort resten av testfallet.
-            while (true)
-            {
-                string next;
-                next = cin.peek();
-                // Kolla om kommande tecken från strömmen är en siffra.
-                if (find_if(next.begin(), next.end(), [](char c) { return !isdigit(c); }) == next.end())
-                    // Ja? - Börja med nästa testfall.
-                    break;
-                    // Nej? - Extrahera.
-                cin.get();
-            }
-            continue;
-        }
-        
-        ++test_count;
-        if (test_count > 500) // För många testfall. Stäng ner programmet.
-            break;
-
         vector<string> students;
     
-        /*
         // Fyll vector students med count st. namn från inläsningsströmmen.
         copy_n(istream_iterator<string>{cin}, count, back_inserter(students));
-        */
-        for (int i = 0; i < count; ++i)
-        {
-            cin >> ws;
-            string name;
-            getline(cin, name);
-            if (!check_name(name))      // Den kanske bara inte ska lägga till namnet om det är felaktigt
-                students.push_back(name);
-        }
-
-        /*
-        // Släng bort testfallet om namnet är fel.
-        if (any_of(students.begin(), students.end(), check_name))
-            continue;
-        */
 
         // Sortera med funktionsobjektet sort_of_sort.
-        sort(students.begin(), students.end(), sort_of_sort);
+        stable_sort(students.begin(), students.end(), sort_of_sort);
 
         cout << delim;
         delim = "\n";
