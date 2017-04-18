8c31e7046a0fd91130f8a743144aba00fc21dad1
Niklas Vågstedt <niklasva@gmail.com>
2017-04-18 18:51:44 +0200

    Assemble.sh + cleanup

--- assemble.sh
+++ assemble.sh
@@ -1,5 +1,7 @@
 if [[ "$1" != "" ]]; then
-    dasm src/$1.s -Iinclude -lbin/$1.lst -sbin/$1.sym -obin/$1.bin -f3 -v5
+    dasm src/$1.asm -Iinclude -lbin/$1.lst -sbin/$1.sym -obin/$1.bin -f3 -v5
+    stella bin/$1.bin &
+    osascript -e 'tell application "Stella" to activate'
 else
     echo "Sorry"
 fi
