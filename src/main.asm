76d2a9725223b71482a40d55fdf3c29569188624
Niklas Vågstedt <niklasva@gmail.com>
2017-04-19 16:26:22 +0200

    🐥

---
+++ src/main.asm
@@ -0,0 +1,28 @@
+
+;;	Assemblerinstruktioner.
+;;	Först och främst måste vi berätta för assemblern 
+;;	att vi programmerar mot 6502 och att vi vill hämta
+;;	definitioner ang. Atari VCS-specifika minnesfunktioner:
+	
+	processor 6502
+	include vcs.h
+	
+	ORG $F000		;; Startadress
+
+;;	Bra jobbat. Nu börjar koden som körs på processorn.
+Start:
+	SEI			;; Set interrupt disable
+	CLD			;; Clear decimal mode
+
+;;	Nollställ stackpekaren:
+	LDX #$FF		;; Fyll register X med FF.
+	TXS			;; Sätt stackpekaren till värdet i X.
+
+;;	Nollställ resten av minnet:
+	LDA #0
+ClearMem:
+	STA 0, X
+	DEX
+	BNE ClearMem
+
+Main:
