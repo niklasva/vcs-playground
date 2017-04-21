f979500b1aa24088889d84a9f99c0f5cb574ee6e
Niklas Vågstedt <niklasva@gmail.com>
2017-04-21 09:32:17 +0200

    bkg

--- src/main.asm
+++ src/main.asm
@@ -14,14 +14,26 @@ Start:
 	CLD			;; Clear decimal mode
 
 ;;	Nollställ stackpekaren:
-	LDX #$FF		;; Fyll register X med FF.
+	LDX #$FF		;; Fyll register X med 0xFF.
 	TXS			;; Sätt stackpekaren till värdet i X.
 
 ;;	Nollställ resten av minnet:
 	LDA #0
 ClearMem:
-	STA 0, X
-	DEX
+	STA 0, X		;; Skriv det som är i register A till minnesplats 0 + X
+	DEX			;; (kom ihåg att vi satte X till FF förut)
 	BNE ClearMem
+
+
+;;	Okej. Kul. Nu kan vi prova att göra lite mer grafiska operationer.
+;;	Sätt bakgrunden till färgen 0x00 (svart)
+
+	LDA #$00		;; Ladda in färgen i A
+	STA COLUBK		;; Skriv värdet i A till bakgrundsfärgregistret
+
+
+;;	Samma med spelare 0 men färg 33
+	LDA #33
+	STA COLUP0
 
 Main:
